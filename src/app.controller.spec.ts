a6457fe7495ceac39a9065ccb9d2ac44dcbe209c
jason taekyun Kim <80494361+taekyunJason@users.noreply.github.com>
2023-06-26 20:37:46 +0900

    test: 사장님 회원가입 테스트 코드 작성

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -19,4 +19,12 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
+  // 사장님 회원가입
+  // 필수값 (아이디/비밀번호/이름/이메일/전화번호/기본주소)을 모두 입력해야 통과
+  describe('owner_signin', () => {
+    it('should done successful signin', () => {
+      expect(appController.getOwnerSignin()).toBe('');
+    });
+  });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  getOwnerSignin(): string {
+    return this.appService.getOwnerSignin();
+  }
 }
