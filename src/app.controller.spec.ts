729a2c7badc7cf3df9f31c3659a840ca475f27be
jason taekyun Kim <80494361+taekyunJason@users.noreply.github.com>
2023-06-26 21:05:07 +0900

    test: 사장님 회원가입 테스트코드 수정 및 typeorm 추가

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { getOwnerSignin } from './app.controller';
+
 describe('AppController', () => {
   let appController: AppController;
 
@@ -19,12 +21,19 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+});
 
-  // 사장님 회원가입
-  // 필수값 (아이디/비밀번호/이름/이메일/전화번호/기본주소)을 모두 입력해야 통과
-  describe('owner_signin', () => {
-    it('should done successful signin', () => {
-      expect(appController.getOwnerSignin()).toBe('');
-    });
+// 사장님 회원가입
+// 필수값 (아이디/비밀번호/이름/이메일/전화번호/기본주소)을 모두 입력해야 통과
+describe('User Registration', () => {
+  it('returns the correct user object after registration', () => {
+    const expectedUser = {
+      id: 'jason11',
+      email: 'jason@gmail.com',
+    };
+
+    const registeredUser = getOwnerSignin();
+
+    expect(registeredUser).toEqual(expectedUser);
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface User {
+  id: string;
+  email: string;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,9 +14,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get()
-  getOwnerSignin(): string {
-    return this.appService.getOwnerSignin();
-  }
 }
+
+function getOwnerSignin(): User {
+  const newUser: User = {
+    id: 'jason11',
+    email: 'jason@gmail.com',
+  };
+
+  return newUser;
+}
+export { getOwnerSignin };
